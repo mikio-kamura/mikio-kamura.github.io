8eddf58def8f079ecf04b39d09f279814c5884b6
Miki Okamura <50087797+mikio-kamura@users.noreply.github.com>
2024-08-10 04:01:26 +0900

    feat: 🎸 blured

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import { Container, Box, Heading } from "@chakra-ui/react";
 
 const Page = () => {
   return (
     <Container>
       <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
-        Hello, I&apos;m mikio
+        Hello, m mikio
       </Box>
 
-      <Box display={{ md: 'flex' }}>
+      <Box display={{ md: "flex" }}>
         <Box flexGrow={1}>
           <Heading as="h2" variant="page-title">
             mikio - Portfolio
@@ -16,7 +16,7 @@ const Page = () => {
         </Box>
       </Box>
     </Container>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
