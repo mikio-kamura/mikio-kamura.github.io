59b0abdaedd47fe2744123939dd9e284be3318bc
mikio（岡村美紀） <hellomiki2027@gmail.com>
2025-03-17 05:07:53 +0900

    feat: 🎸 ogp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,19 @@
 import P5Sketch from '../components/P5Sketch';
 import style from "./page.module.css";
 
+export const metadata = {
+  openGraph: {
+    title: "hand manipulate hand",
+    description:
+      "made by mikio, with p5.js",
+    images: [{
+      url: "/ogp/ogp.png", // 🌟 静的画像の指定
+      width: 1200,
+      height: 630
+    }],
+  },
+};
+
 export default function Home() {
   return (
     <div className={style.page}>
